aa504be57dfd6815a7b22ad6f9424875d02a0a79
Eduardo Maciel <138815948+leicam1995@users.noreply.github.com>
2024-05-24 11:45:06 -0300

    Arquivos de dados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,20 +1,22 @@
 /// <reference types="cypress"/>
 
+const perfil = require('../../fixtures/perfil.json')
+
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
         cy.screenshot()
     });
 
-    it('Deve fazer login com sucesso', () => { 
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('Du.teste')
         cy.get('#password').type('123456')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, du.teste (não é du.teste? Sair)')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, du.teste (não é du.teste? Sair)')
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuário invalido', () => {
@@ -31,4 +33,25 @@ describe('Funcionalidade: Login', () => {
         cy.get('.woocommerce-error').should('contain', 'Erro: A senha informada para o usuário Du.teste está incorreta. Perdeu a senha?')
         cy.get('.woocommerce-error').should('exist')
     });
+
+    it('Deve fazer login com sucesso usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, du.teste (não é du.teste? Sair)')
+
+
+    });
+
+    it.only('Deve fazer login com sucesso usando fixture', () => {
+        cy.fixture('perfil').then(dados => {
+
+            cy.get('#username').type(dados.usuario , {log: false})
+            cy.get('#password').type(dados.senha , {log: false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, du.teste (não é du.teste? Sair)')
+        })
+
+    });
 })
+
--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -3,7 +3,7 @@
 describe('Funcionalidade: produtos', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        cy.visit('produtos')
     });
 
 
